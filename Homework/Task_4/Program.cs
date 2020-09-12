1dcc56e3da2bed3303c224bfc2d7345cec2f6184
twice <twice@example.com>
2020-09-12 16:12:42 +0300

    Task 4 done

---
+++ Homework/Task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(CountWords("one word two word"));
+        }
+
+        static int CountWords(string str)
+        {
+            return str.Split().Length;
+        }
+    }
+}
