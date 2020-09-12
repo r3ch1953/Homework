0f2cc55ec0f980068259a08e48750639e9d71af5
twice <twice@example.com>
2020-09-12 16:02:29 +0300

    Task 3 done

---
+++ Homework/Task_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter string: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Is palindrom: " + IsPalindrom(str));
+        }
+
+        static bool IsPalindrom(string str)
+        {
+            return str.ToLower() == ReverseString(str).ToLower();
+        }
+
+        static string ReverseString(string str)
+        {
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
