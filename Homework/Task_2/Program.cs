e78648257bbd30ada8d8102343e50d00924f5c65
twice <twice@example.com>
2020-09-15 18:07:57 +0300

    Task 2 done

---
+++ Homework/Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(11, 15, '#');
+            rect.Draw();
+
+            Console.WriteLine('\n' + new String('-', 30) + '\n');
+
+            rect.Draw(ConsoleColor.Green);
+
+            Console.WriteLine('\n' + new String('-', 30) + '\n');
+
+            Console.WriteLine($"Square: {rect.Square()}");
+        }
+    }
+}
---
+++ Homework/Task_2/Rectangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Rectangle
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public char Symbol { get; set; }
+
+        public Rectangle()
+        {
+            Height = 10;
+            Width = 10;
+            Symbol = '#';
+        }
+
+        public Rectangle(int height = 10, int width = 10, char symbol = '#')
+        {
+            Height = height;
+            Width = width;
+            Symbol = symbol;
+        }
+
+        public int Square()
+        {
+            return Height * Width;
+        }
+
+        public void Draw()
+        {
+            for(int i = 0; i < Height; i++)
+            {
+                for(int j = 0; j < Width; j++)
+                {
+                    Console.Write(Symbol + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Draw(ConsoleColor color = ConsoleColor.White)
+        {
+            Console.BackgroundColor = color;
+            Console.ForegroundColor = color;
+
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write(Symbol + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
