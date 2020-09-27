07f71d320b469419cce0f9923f166ef520a4d1a0
twice <twice@example.com>
2020-09-27 14:18:35 +0300

    Task 1 done

---
+++ Homework/Task_1/LinearEquation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class LinearEquation
+    {
+        public int CoeffA { get; set; }
+        public int CoeffB { get; set; }
+
+        public LinearEquation(int coeffA, int coeffB)
+        {
+            if(coeffA == 0 && CoeffB == 0)
+                throw new FormatException("Coefficients A and B are zero");
+            else if (coeffA == 0)
+                throw new FormatException("Coefficient A is zero");
+            else if (coeffB == 0)
+                throw new FormatException("Coefficient B is zero");
+
+            CoeffA = coeffA;
+            CoeffB = coeffB;
+        }
+
+        public int Slove()
+        {
+            return -CoeffB / CoeffA;
+        }
+
+        public override string ToString()
+        {
+            return $"{CoeffA} * X + ({CoeffB}) = 0";
+        }
+
+        public static LinearEquation Parse(string args)
+        {
+            var coeffs = args.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            try
+            {
+                LinearEquation linear = new LinearEquation(int.Parse(coeffs[0]), int.Parse(coeffs[1]));
+                return linear;
+            }
+            catch
+            {
+                throw;
+            }           
+        }
+    }
+}
---
+++ Homework/Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                LinearEquation linearEquation = LinearEquation.Parse("10 0");
+                Console.WriteLine(linearEquation);
+                Console.WriteLine("X = " + linearEquation.Slove());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
