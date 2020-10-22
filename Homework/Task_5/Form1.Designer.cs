63aee5a7a8db4e86f9d45f9bdda98558cb214fb4
twice <twice@example.com>
2020-10-22 17:15:07 +0300

    Task 5 done

---
+++ Homework/Task_5/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Task_5
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.Panel = new System.Windows.Forms.Panel();
+            this.Label = new System.Windows.Forms.Label();
+            this.PanelTimer = new System.Windows.Forms.Timer(this.components);
+            this.Panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(13, 415);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(95, 414);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 1;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Timer
+            // 
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Panel
+            // 
+            this.Panel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Panel.Controls.Add(this.Label);
+            this.Panel.Location = new System.Drawing.Point(10, 10);
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(80, 80);
+            this.Panel.TabIndex = 3;
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Location = new System.Drawing.Point(22, 36);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(33, 13);
+            this.Label.TabIndex = 0;
+            this.Label.Text = "0 sec";
+            // 
+            // PanelTimer
+            // 
+            this.PanelTimer.Tick += new System.EventHandler(this.PanelTimer_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Panel);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Task_5";
+            this.Panel.ResumeLayout(false);
+            this.Panel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Panel Panel;
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.Timer PanelTimer;
+    }
+}
+
---
+++ Homework/Task_5/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_5
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            PanelTimer.Interval = 1;
+            Timer.Interval = 1000;
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            Panel.Location = new Point(20, 20);
+            Timer.Stop();
+            PanelTimer.Stop();
+            MessageBox.Show($"Time {i} sec", "Timer", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            Timer.Start();
+            PanelTimer.Start();
+        }
+
+        int i = 0;
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Label.Text = $"{++i} sec";
+        }
+
+        private void GoRight()
+        {
+            Panel.Location = new Point(Panel.Location.X + 1, Panel.Location.Y);
+        }
+
+        private void GoDown()
+        {
+            Panel.Location = new Point(Panel.Location.X, Panel.Location.Y + 1);
+        }
+
+        private void GoUp()
+        {
+            Panel.Location = new Point(Panel.Location.X, Panel.Location.Y - 1);
+        }
+
+        private void GoLeft()
+        {
+            Panel.Location = new Point(Panel.Location.X - 1, Panel.Location.Y);
+        }
+
+        bool[] done = { false, false, false, false };
+        private void PanelTimer_Tick(object sender, EventArgs e)
+        {
+            int border = 20;
+
+            if (Panel.Location.X + Panel.Width != Width - border && !done[0])
+            {
+                GoRight();
+                done[0] = (Panel.Location.X + Panel.Width) == (Width - border) ? true : false;
+            }
+
+            if (Panel.Location.Y + Panel.Height != Height - border && done[0] && !done[1])
+            {
+                GoDown();
+                done[1] = (Panel.Location.Y + Panel.Height) == (Height - border * 2) ? true : false;
+            }
+
+            if (Panel.Location.X != border && done[0] && done[1] && !done[2])
+            {
+                GoLeft();
+                done[2] = (Panel.Location.X) == (border) ? true : false;
+            }
+
+            if (Panel.Location.Y != border && done[0] && done[1] && done[2] && !done[3])
+            {
+                GoUp();
+                done[3] = (Panel.Location.Y) == (border) ? true : false;
+            }
+
+            if (done[3])
+            {
+                Array.Clear(done, 0, done.Length);
+            }
+        }
+    }
+}
+
