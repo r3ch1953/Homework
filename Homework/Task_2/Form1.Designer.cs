fa25b2a5a9b299943ef12edf3af5e32daa3f1595
twice <twice@example.com>
2020-10-23 15:40:26 +0300

    Task 2 done

---
+++ Homework/Task_2/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Task_2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(88, 138);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(32, 13);
+            this.label.TabIndex = 0;
+            this.label.Text = "static";
+            this.label.MouseHover += new System.EventHandler(this.label_MouseHover);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label);
+            this.Name = "MainForm";
+            this.Text = "Task_2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ Homework/Task_2/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label_MouseHover(object sender, EventArgs e)
+        {
+            var rand = new Random();
+            label.Location = new Point(rand.Next(0, ClientSize.Width - label.Width), rand.Next(0, ClientSize.Height - label.Height));
+        }
+    }
+}
