fda67d32bc8bd038577a2bfafa29607024cc1c17
twice <twice@example.com>
2020-10-26 12:28:37 +0200

    Task 3 done

---
+++ Homework/Task_3/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            var res = dateTimePicker1.Value.Date - dateTimePicker2.Value.Date;
+            label1.Text = $"Res -> {res.TotalDays} days";
+        }
+    }
+}
